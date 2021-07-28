9b8e498f9624c61ff303fa70d95c3feb10d603ee
LingyiGao <2511641g@student.gla.ac.uk>
2021-07-28 14:47:53 +0100

    char5

--- rango/admin.py
+++ rango/admin.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
-from django.db import models
+#can be delelet
+
+#can be delelet
 from rango.models import Category, Page
 # Register your models here.
+
+class PageAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'url')
 admin.site.register(Category)
-admin.site.register(Page)
+
+admin.site.register(Page, PageAdmin)
 
 
