5991a3d479a8f57439b4527da5ea01d0b190c8d7
LingyiGao <2511641g@student.gla.ac.uk>
2021-07-29 16:00:58 +0100

    char8

--- rango/views.py
+++ rango/views.py
@@ -62,7 +62,7 @@ def add_page(request, category_name_slug):
     
     
     if category is None:
-        return redirect('/rango/')
+        return redirect(reverse('rango:index'))
 
     form = PageForm()
 
